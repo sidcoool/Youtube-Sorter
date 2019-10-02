c0b814e3da6cdd9383032d349882b8f608f239ba
sid <siddharthagoel1998@gmail.com>
2019-10-02 18:51:03 +0530

    Basic likes sorting

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { apikey } from '../environments/environment'
+import { video } from '../environments/environment'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  constructor(private http: HttpClient) { }
+
+  search: boolean = true
+  n_search = ""
+  p_search = "dwdwd"
+  videos: video[] = []
+
+  //videos: {id:string, pic:string} = []
+
+  searchTrue = () => {
+    this.search = true
+  }
+
+  searchFalse = () => {
+    this.search = false
+  }
+
+  nsearch = () => {
+    console.log(this.n_search)
+
+    this.http.get<any>(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.n_search}&type=video&key=${apikey}`)
+    .subscribe((data) => {
+      console.log(data)
+      this.videos = []
+
+      for(let item of data.items){
+        
+
+        this.http.get<any>(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${item.id.videoId}&key=${apikey}`)
+        .subscribe((data) => {
+          //data.item[0].statistics.likeCount
+          this.videos.push({
+            like: data.items[0].statistics.likeCount,
+            pic: item.snippet.thumbnails.medium.url,
+            title: item.snippet.title
+          })
+        })
+      }
+    })
+  }
+
+  sortByLikes = () => {
+    this.videos.sort((a:video, b: video) => {
+      return (a.like > b.like ? 1 : -1)
+    })
+  }
+
+  psearch = () => {
+    console.log(this.p_search)
+  }
+}
