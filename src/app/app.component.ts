e7b736f971ab296f963e3d3e97046b67ff2c97ee
sid <siddharthagoel1998@gmail.com>
2019-10-06 21:15:06 +0530

    view and download feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { apikey } from '../../environment.prod'
 import { video } from '../../environment.prod'
 
@@ -8,7 +8,6 @@ import { video } from '../../environment.prod'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 
 
 export class AppComponent {
@@ -28,33 +27,47 @@ export class AppComponent {
   searchFalse = () => {
     this.search = false
   }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'text/html',
+    })
+  };
 
   nsearch = () => {
     console.log(this.n_search)
 
     this.http.get<any>(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.n_search}&type=video&key=${apikey}`)
-    .subscribe((data) => {
-      console.log(data)
-      this.videos = []
+      .subscribe((data) => {
+        console.log(data)
+        this.videos = []
 
-      for(let item of data.items){
-        
+        for (let item of data.items) {
 
-        this.http.get<any>(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${item.id.videoId}&key=${apikey}`)
-        .subscribe((data) => {
-          //data.item[0].statistics.likeCount
-          this.videos.push({
-            like: data.items[0].statistics.likeCount,
-            pic: item.snippet.thumbnails.medium.url,
-            title: item.snippet.title   
-          })
-        })
-      }
-    })
+
+          this.http.get<any>(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${item.id.videoId}&key=${apikey}`)
+            .subscribe((data) => {
+              //data.item[0].statistics.likeCount
+              this.videos.push({
+                link: item.id.videoId,
+                like: data.items[0].statistics.likeCount,
+                pic: item.snippet.thumbnails.medium.url,
+                title: item.snippet.title
+              })
+            })
+        }
+      })
+  }
+  //IQIZqpI1-8g
+  sortByLikes = () => {
+    this.videos.sort((a: video, b: video) => b.like - a.like)
   }
 
-  sortByLikes = () => {
-    this.videos.sort((a:video, b: video) => b.like - a.like)
+  linkOpen = (link) => {
+    window.open(`https://youtube.com/watch?v=${link}`)
+  }
+
+  downloadVideo = (link) => {
+    window.open(`https://ysapp.herokuapp.com/download/url?a=https://www.youtube.com/watch?v=${link}`, '_blank');
   }
 
   psearch = () => {
---
+++ src/app/test.js
@@ -0,0 +1,4 @@
+
+function shs(){
+    console.log("yeah !")
+}
